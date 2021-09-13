ab55f2e47c1146e7a4a38fd8cb853d45d73573c8
Douglas <douglaskerins@gmail.com>
2021-09-13 19:51:55 +0100

    Add student_count variable and refactoring

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -6,14 +7,11 @@ puts "Darth Vader"
 puts "Nurse Ratched"
 puts "Michael Corleone"
 puts "Alex DeLarge"
-puts ;"The Wicked Witch of the West"
+puts "The Wicked Witch of the West"
 puts "Terminator"
 puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # finally, we print the total number of students
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
