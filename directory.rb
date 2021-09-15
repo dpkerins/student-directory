7b14e0ce20bc8525c3748925cd2a0a6a5e3c452f
Douglas <douglaskerins@gmail.com>
2021-09-15 10:52:41 +0100

    Refactor Interactive Menu and make instance variable students

--- directory.rb
+++ directory.rb
@@ -1,25 +1,35 @@
+@students = []
+
 def interactive_menu
-  students = []
-
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
+    print_menu
     selection = gets. chomp
+    process(selection)
+  end
+end
 
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
   end
 end
 
@@ -32,13 +42,12 @@ def input_students
   #while name is not empty repeat this code
   while !name.empty? do 
     #add the student to the hash array
-    students << {name: name, cohort: "november"}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: "november"}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
   #return the array of students
-  students
 end
 
 def print_header
@@ -46,14 +55,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 #nothing happens until we call the methods
